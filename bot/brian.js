aef0752eff57a4db1510e3ffd0219b4ddda0953e
Brian LeVee <brianlevee@gmail.com>
2016-06-04 15:52:23 -0400

    basic messaging working 2

--- bot/brian.js
+++ bot/brian.js
@@ -1,31 +1,46 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var server = restify.createServer();
-
-var helloBot = new builder.BotConnectorBot();
-helloBot.add('/', new builder.CommandDialog()
-    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
-    .matches('^quit', builder.DialogAction.endDialog())
-    .onDefault([
-        function (session, args, next) {
-            if (!session.userData.name) {
-                session.beginDialog('/profile');
-            } else {
-                next();
-            }
-        },
-        function (session, results) {
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'apartmentbot', appSecret: '00eed7d3f1404af89d9f9dad303453ee' });
+// bot.add('/', function (session) {
+//     session.send('Hello World');
+// });
+bot.add('/', [
+    function (session, args, next) { //session.send("name:%s, price:%d", session.userData.name, session.userData.price);
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } 
+        else if (!session.userData.price) {
+            session.beginDialog('/price');
+        }
+        else if (!session.userData.rooms) {
+            session.beginDialog('/rooms');
+        }
+        else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (session.userData.name && !session.userData.price) {
             session.send('Hello %s!', session.userData.name);
+            session.beginDialog('/price');
+        } 
+        //else if (session.userData.price && !session.userData.rooms) {
+        //    session.send('Hello %s! I\'ll find you an apartment for less than $%d per month', session.userData.name, session.userData.price); 
+        //}
+        else if (session.userData.rooms && session.userData.rooms && session.userData.price) {
+             session.send('Hello %s! I\'ll find you an apartment for less than $%d per month with %d rooms', session.userData.name, session.userData.price, session.userData.rooms);    
         }
-    ]));
-helloBot.add('/profile',  [
+        else {
+            session.send('crap...'); 
+        }
+        
+    }
+]);
+bot.add('/profile', [
     function (session) {
-        if (session.userData.name) {
-            builder.Prompts.text(session, 'What would you like to change it to?');
-        } else {
-            builder.Prompts.text(session, 'Hi! What is your name?');
-        }
+        builder.Prompts.text(session, 'Hi! What is your name?');
     },
     function (session, results) {
         session.userData.name = results.response;
@@ -33,9 +48,31 @@ helloBot.add('/profile',  [
     }
 ]);
 
-server.use(helloBot.verifyBotFramework({ appId: 'apartmentbot', appSecret: '00eed7d3f1404af89d9f9dad303453ee' }));
-server.post('/api/messages', helloBot.listen());
+bot.add('/price', [
+    function (session) {
+        builder.Prompts.text(session, "What's the most you'd want to pay per month?");
+    },
+    function (session, results) {
+        session.userData.price = results.response;
+        session.beginDialog('/');
+    //    session.endDialog();
+    }
+]);
 
-server.listen(3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+bot.add('/rooms', [
+    function (session) {
+        builder.Prompts.text(session, "How many bedrooms do you need?");
+    },
+    function (session, results) {
+        session.userData.rooms = results.response;
+        session.beginDialog('/');
+    //    session.endDialog();
+    }
+]);
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
