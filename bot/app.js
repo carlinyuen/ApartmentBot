571915cda6645ba2d0f54fb4993efdfc3b514aed
Carlin <email.me@carlinyuen.com>
2016-06-04 10:45:56 -0400

    set up name storing

--- bot/app.js
+++ bot/app.js
@@ -3,9 +3,30 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
-    session.send('Hello World');
-});
+// bot.add('/', function (session) {
+//     session.send('Hello World');
+// });
+bot.add('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
