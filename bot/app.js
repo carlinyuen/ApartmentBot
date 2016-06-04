07c47753069896bc66f19df229523150d439a486
Carlin <email.me@carlinyuen.com>
2016-06-04 16:41:00 -0400

    app.js is brian's

--- bot/app.js
+++ bot/app.js
@@ -7,15 +7,35 @@ var bot = new builder.BotConnectorBot({ appId: 'apartmentbot', appSecret: '00eed
 //     session.send('Hello World');
 // });
 bot.add('/', [
-    function (session, args, next) {
+    function (session, args, next) { //session.send("name:%s, price:%d", session.userData.name, session.userData.price);
         if (!session.userData.name) {
             session.beginDialog('/profile');
-        } else {
+        } 
+        else if (!session.userData.price) {
+            session.beginDialog('/price');
+        }
+        else if (!session.userData.rooms) {
+            session.beginDialog('/rooms');
+        }
+        else {
             next();
         }
     },
     function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+        if (session.userData.name && !session.userData.price) {
+            session.send('Hello %s!', session.userData.name);
+            session.beginDialog('/price');
+        } 
+        //else if (session.userData.price && !session.userData.rooms) {
+        //    session.send('Hello %s! I\'ll find you an apartment for less than $%d per month', session.userData.name, session.userData.price); 
+        //}
+        else if (session.userData.rooms && session.userData.rooms && session.userData.price) {
+             session.send('Hello %s! I\'ll find you an apartment for less than $%d per month with %d rooms', session.userData.name, session.userData.price, session.userData.rooms);    
+        }
+        else {
+            session.send('crap...'); 
+        }
+        
     }
 ]);
 bot.add('/profile', [
@@ -28,15 +48,31 @@ bot.add('/profile', [
     }
 ]);
 
-exports.handler = function(event, context) {
-    console.log("Initalized Bot...");
-    console.log(event);
-    context.succeed("Hello World!");
-};
+bot.add('/price', [
+    function (session) {
+        builder.Prompts.text(session, "What's the most you'd want to pay per month?");
+    },
+    function (session, results) {
+        session.userData.price = results.response;
+        session.beginDialog('/');
+    //    session.endDialog();
+    }
+]);
+
+bot.add('/rooms', [
+    function (session) {
+        builder.Prompts.text(session, "How many bedrooms do you need?");
+    },
+    function (session, results) {
+        session.userData.rooms = results.response;
+        session.beginDialog('/');
+    //    session.endDialog();
+    }
+]);
 
 // Setup Restify Server
-// var server = restify.createServer();
-// server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-// server.listen(process.env.port || 3978, function () {
-//     console.log('%s listening to %s', server.name, server.url);
-// });
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
