a07cb90e69d82d3f66bac239606bb76d16e9d99e
Carlin <email.me@carlinyuen.com>
2016-06-05 12:11:08 -0400

    switching back to carlin number

--- bot/app.js
+++ bot/app.js
@@ -15,7 +15,7 @@ var bot = new builder.BotConnectorBot({ appId: 'apartmentbot', appSecret: '00eed
 // });
 
 var stopMe = false;
-var blvNumber = '+13107045798';
+var carlinNumber = '+12345679763';
 var twilioNumber = '+12343086132';
 
 var myFunction = function() {}
@@ -605,30 +605,36 @@ bot.add('/number', [
 
             // Send an SMS text message
             client.sendMessage({
-              to:blvNumber, from:twilioNumber,
+              to:carlinNumber, from:twilioNumber,
               body: 'Brokerless friend in need!',
             }, function(err, responseData) {
               if (!err) {
                 console.log(responseData.from);
                 console.log(responseData.body);
+              } else {
+                console.log(err);
               }
             });
             client.sendMessage({
-              to:blvNumber, from:twilioNumber,
+              to:carlinNumber, from:twilioNumber,
               body: 'Contact #: ' + session.userData.number,
             }, function(err, responseData) {
               if (!err) {
                 console.log(responseData.from);
                 console.log(responseData.body);
+              } else {
+                console.log(err);
               }
             });
             client.sendMessage({
-              to:blvNumber, from:twilioNumber,
+              to:carlinNumber, from:twilioNumber,
               body: 'Addy: ' + session.userData.address,
             }, function(err, responseData) {
               if (!err) {
                 console.log(responseData.from);
                 console.log(responseData.body);
+              } else {
+                console.log(err);
               }
             });
         }
